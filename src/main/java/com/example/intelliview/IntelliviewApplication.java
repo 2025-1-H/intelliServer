00ff57b24681aa8ce7aef7b34fc2af2f62a49b6f
junstory <lastcom1@gmail.com>
2025-04-15 15:15:28 +0900

    init

---
+++ src/main/java/com/example/intelliview/IntelliviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.intelliview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelliviewApplication.class, args);
+	}
+
+}
