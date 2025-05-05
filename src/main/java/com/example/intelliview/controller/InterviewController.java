4a3333cbae12827eaddaf43e0ff2789837d13b71
minahkim03 <minahkim026@gmail.com>
2025-05-06 04:43:00 +0900

    Feat. Bedrock 질문 생성 기능 구현

--- src/main/java/com/example/intelliview/controller/InterviewController.java
+++ src/main/java/com/example/intelliview/controller/InterviewController.java
@@ -1,12 +1,19 @@
 package com.example.intelliview.controller;
 
+import com.example.intelliview.domain.Question;
 import com.example.intelliview.dto.interview.InterviewInfoDto;
+import com.example.intelliview.dto.interview.InterviewQuestionsDto;
 import com.example.intelliview.service.InterviewService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.ArrayList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,8 +24,12 @@ public class InterviewController {
     private final InterviewService interviewService;
 
     @PostMapping("/info")
-    public ResponseEntity<String> getInterviewInfo(@RequestBody InterviewInfoDto interviewInfoDto) {
-        interviewService.getInterviewInfo(interviewInfoDto);
-        return ResponseEntity.ok("정보 입력 완료");
+    public ResponseEntity<Long> getInterviewInfo(@RequestBody InterviewInfoDto interviewInfoDto) {
+        return ResponseEntity.ok(interviewService.getInterviewInfo(interviewInfoDto));
+    }
+
+    @GetMapping("/{id}/start")
+    public ResponseEntity<InterviewQuestionsDto> startInterview(@PathVariable Long id) throws JsonProcessingException {
+        return ResponseEntity.ok(interviewService.startInterview(id));
     }
 }
--- src/main/java/com/example/intelliview/service/InterviewService.java
+++ src/main/java/com/example/intelliview/service/InterviewService.java
@@ -3,9 +3,13 @@ package com.example.intelliview.service;
 import com.example.intelliview.domain.Interview;
 import com.example.intelliview.domain.InterviewStatus;
 import com.example.intelliview.domain.Member;
+import com.example.intelliview.domain.Question;
 import com.example.intelliview.dto.interview.InterviewInfoDto;
+import com.example.intelliview.dto.interview.InterviewQuestionsDto;
 import com.example.intelliview.repository.InterviewRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.transaction.Transactional;
+import java.util.ArrayList;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,8 +21,9 @@ import org.springframework.stereotype.Service;
 public class InterviewService {
 
     private final InterviewRepository interviewRepository;
+    private final BedrockService bedrockService;
 
-    public void getInterviewInfo(InterviewInfoDto interviewInfoDto) {
+    public Long getInterviewInfo(InterviewInfoDto interviewInfoDto) {
         Interview interview = Interview.builder()
             .status(InterviewStatus.SCHEDULED)
             .occupation(interviewInfoDto.getOccupation())
@@ -26,5 +31,14 @@ public class InterviewService {
             .member(new Member())
             .build();
         interviewRepository.save(interview);
+        return interview.getId();
+    }
+
+    public InterviewQuestionsDto startInterview(Long id) throws JsonProcessingException {
+        Interview interview = interviewRepository.findById(id).orElseThrow();
+        interview.updateStatus(InterviewStatus.IN_PROGRESS);
+        return InterviewQuestionsDto.builder()
+            .questions(bedrockService.generateInterviewQuestions(interview))
+            .build();
     }
 }
