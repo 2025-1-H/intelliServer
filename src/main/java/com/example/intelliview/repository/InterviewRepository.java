ea531baacadd75a2a9d1268cd44b1193475dd490
minahkim03 <minahkim026@gmail.com>
2025-06-11 00:07:42 +0900

    Feat. 면접 피드백 목록 반환 기능 추가

--- src/main/java/com/example/intelliview/repository/InterviewRepository.java
+++ src/main/java/com/example/intelliview/repository/InterviewRepository.java
@@ -1,8 +1,12 @@
 package com.example.intelliview.repository;
 
 import com.example.intelliview.domain.Interview;
+import com.example.intelliview.domain.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InterviewRepository extends JpaRepository<Interview, Long> {
+
+    public List<Interview> findByMember(Member member);
 
 }
